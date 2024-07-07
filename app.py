afa160856baf411eb145dbff764c31ee3659a019
Orion Weller <wellerorion@gmail.com>
2024-07-06 20:13:07 -0400

    working

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from leaderboard import build_leaderboard_tab
 from models import ModelManager
 from ui import build_side_by_side_ui_anon, build_side_by_side_ui_anon_sts, build_side_by_side_ui_anon_clustering, build_side_by_side_ui_named, build_side_by_side_ui_named_sts, build_side_by_side_ui_named_clustering, build_single_model_ui, build_single_model_ui_sts, build_single_model_ui_clustering
 
+from arena_elo.elo_analysis import load_results
+
 
 # download the videos 
 from huggingface_hub import hf_hub_url
@@ -49,13 +51,13 @@ def load_elo_results(elo_results_dir):
         elo_results_dir = Path(elo_results_dir)
         elo_results_file = {}
         leaderboard_table_file = {}
-        for file in elo_results_dir.glob('elo_results_*.pkl'):
+        for folder in elo_results_dir.glob('elo_results_*'):
             if 'clustering' in file.name:
-                elo_results_file['clustering'] = file
+                elo_results_file['clustering'] = load_results(folder)
             elif 'retrieval' in file.name:
-                elo_results_file['retrieval'] = file
+                elo_results_file['retrieval'] = load_results(folder)
             elif 'sts' in file.name:
-                elo_results_file['sts'] = file
+                elo_results_file['sts'] = load_results(folder)
             else:
                 raise ValueError(f"Unknown file name: {file.name}")
         for file in elo_results_dir.glob('*_leaderboard.csv'):
--- arena_elo/update_elo.sh
+++ arena_elo/update_elo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mkdir -p results
-
+mkdir -p results/latest
 # For battle data
+
 for task in "retrieval" "clustering" "sts"; do
     python -m arena_elo.clean_battle_data --task_name $task
     battle_cutoff_date=`cat cut_off_date.txt` && rm cut_off_date.txt && echo "$task battle data last updated on $battle_cutoff_date"
@@ -10,13 +11,15 @@ for task in "retrieval" "clustering" "sts"; do
     cp clean_battle_${task}_$battle_cutoff_date.json ./results/latest/clean_battle_$task.json
     mv clean_battle_${task}_$battle_cutoff_date.json ./results/$battle_cutoff_date/clean_results_${task}.json
     python3 -m arena_elo.elo_analysis --clean-battle-file ./results/$battle_cutoff_date/clean_results_${task}.json --num-bootstrap 1
-    mv ./elo_results_$battle_cutoff_date.pkl ./results/$battle_cutoff_date/elo_results_${task}.pkl
-    python -m arena_elo.generate_leaderboard \
-        --elo_rating_pkl "./results/$battle_cutoff_date/elo_results_${task}.pkl" \
-        --output_csv "./results/$battle_cutoff_date/${task}_leaderboard.csv"
+    mv ./elo_results_$battle_cutoff_date ./results/$battle_cutoff_date/elo_results_${task}
+    cmd="""python -m arena_elo.generate_leaderboard \
+        --elo_rating_folder "./results/$battle_cutoff_date/elo_results_${task}" \
+        --output_csv "./results/$battle_cutoff_date/${task}_leaderboard.csv""""
+    echo $cmd
+    eval $cmd
     mkdir -p ./results/latest
     cp ./results/$battle_cutoff_date/${task}_leaderboard.csv ./results/latest/${task}_leaderboard.csv
-    cp ./results/$battle_cutoff_date/elo_results_${task}.pkl ./results/latest/elo_results_${task}.pkl
+    cp -R ./results/$battle_cutoff_date/elo_results_${task} ./results/latest/elo_results_${task}
     echo "$task leaderboard updated"
 done
 
