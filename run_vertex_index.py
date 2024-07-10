6866fe3b5dafcb488e66cbe88d3cdcb5792e64f6
Muennighoff <n.muennighoff@gmail.com>
2024-07-10 04:30:31 +0000

    Automate

--- run_vertex_index.py
+++ run_vertex_index.py
@@ -1,13 +1,7 @@
+"""Script for testing the VertexIndex code and setup the vector search index."""
+import sys
 from yaml import safe_load
-
 from models import ModelManager
-
-"""
-Script for testing the VertexIndex code and setup the vector search index.
-
-Download corpus.jsonl by running
->>> wget https://huggingface.co/datasets/mteb/nq/resolve/main/corpus.jsonl
-"""
 
 MODEL_META_PATH = "model_meta.yml"
 with open(MODEL_META_PATH, 'r', encoding='utf-8') as f:
@@ -16,10 +10,7 @@ with open(MODEL_META_PATH, 'r', encoding='utf-8') as f:
 ## Enable GCP Vertex index
 models = ModelManager(model_meta, use_gcp_index=True)
 
+model_name = sys.argv[1] # "intfloat/multilingual-e5-small"
+corpus = sys.argv[2] # "wikipedia"
 
-## Test with a small model
-#model_name = "intfloat/e5-mistral-7b-instruct"
-model_name = "GritLM/GritLM-7B"
-#model_name = "BAAI/bge-large-en-v1.5"
-#model_name = "intfloat/multilingual-e5-small"
-print(models.retrieve(query="Where is Japan?", model_name=model_name))
+print(models.retrieve(query="Where is Japan?", model_name=model_name, corpus=corpus))
